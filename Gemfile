2d2a23a2e3f69b886e0dac97928b8e8807b8f6e8
Mike <mdatko@sbcglobal.net>
2018-02-04 22:43:23 +0000

    finished finance tracker tutorial

--- Gemfile
+++ Gemfile
@@ -43,7 +43,7 @@ gem 'devise-bootstrap-views'
 
 gem 'stock_quote'
 
-
+gem 'font-awesome-rails'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,12 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'my_portfolio', to: "users#my_portfolio"
   get 'search_stocks', to: "stocks#search"
+  get 'my_friends', to: "users#my_friends"
   resources :user_stocks, only: [:create, :destroy]
+  resources :users, only: [:show]
+  resources :friendships
+  get 'search_friends', to: 'users#search'
+  post 'add_friend', to:'users#add_friend'
   
   
   
